4cb2c4367b3bfcb81ed1acd7c4155992f22dbe34
ronnysc70 <ronnys30@gmx.de>
2024-11-20 07:57:56 +0100

    Add files via upload
    wifi ausgelagert aus main, Wifi Check nicht bei Stationswahl

--- webradio.ino
+++ webradio.ino
@@ -135,37 +135,6 @@ void onOTAEnd(bool success)
   }
 }
 
-//init WiFi Verbindung
-int setup_wifi()
-{       
-  Serial.println("Connecting to WiFi");
-  WiFi.disconnect();
-  WiFi.softAPdisconnect(true);
-  if (tryCount < MAXWLANTRY) {
-    // Mit Wi-Fi verbinden
-    Serial.println("...Connecting to WiFi");
-    lcdPrint(0, 0, "suche WLAN...");
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(SSID, PSK);
-     while ((WiFi.status() != WL_CONNECTED) && (tryCount<MAXWLANTRY)) 
-     {
-       Serial.print(".");
-       lcdPrint(tryCount, 1, ">");
-       delay(1000);
-       tryCount++;
-    }
-    IPAddress lip(0,0,0,0);
-    if (WiFi.localIP() == lip) {
-      tryCount = MAXWLANTRY;
-    }
-  }
-  if (tryCount < MAXWLANTRY) {
-    Serial.print(F("IP-Adresse per DHCP ist "));
-    Serial.println(WiFi.localIP());
-    return(true);   // when connection ok, stop here and return positive
-  }
-  return(false);
-}
 
 
 
@@ -264,6 +233,10 @@ void loop()
     {
       audio_loop();
       rotary_loop();
+      if (!(btnStation))        //wenn Senderwahl keine Abfrage des Wifi
+      {
+        wifi_loop();
+      }
     }
     else        //Standby gewählt
     {
@@ -271,73 +244,6 @@ void loop()
       {
         delayTimeRefresh = millis();
         showStandby();
-      }
-    }
-    if ((millis() - delayTimeRefresh) > 2000)       //aller 2 Sekunden Überprüfung WLAN und Internet Verbindung
-    {
-      int retry = 0;
-      delayTimeRefresh = millis();
-      if (WiFi.status() != WL_CONNECTED)
-      {
-        Serial.println("reconnect WIFI");
-        lcd.clear();
-        lcdPrint(0, 0, "Huch, WLAN weg..");
-        lcdPrint(0, 1, "...verbinde neu");
-        WiFi.disconnect();
-        WiFi.softAPdisconnect(true);
-        WiFi.mode(WIFI_STA);
-        WiFi.begin(SSID, PSK);
-        while (WiFi.status() != WL_CONNECTED)
-        {
-          Serial.print(".");
-          delay(100);
-        }
-        if (btnStandby)
-        {
-          showStandby();
-        }
-        else
-        {
-          showStation();
-        }
-      }
-      // teste ob eine Verbindung zum Internet besteht wg. capitive Portal
- 
-      else
-      {
-        while((!httpsClient.connect("www.google.de", 443, 2000)) && (retry < 5))  //3. Wert ist timeout der Verbindung in ms
-        {
-          delay(100);
-          Serial.print("<*>");
-          retry++;   
-        }
-        if (retry==5)
-        {
-          Serial.println("keine Verbindung");
-          lcd.clear();
-          lcdPrint(0, 0, "kein Internet..");
-          lcdPrint(0, 1, "..neu anmelden");
-          internetLost = true;
-        }
-        else
-        {
-          Serial.println("Internet erreichbar");
-          if (internetLost)
-          {
-            internetLost = false;
-            Serial.println("Internet wieder erreichbar, starte Stream neu");
-            if (btnStandby)
-            {
-              showStandby();
-            }
-            else
-            {
-              showStation();
-              startUrl();
-            }
-          }
-        }
-        httpsClient.stop();
       }
     }
     //read events from buttons
